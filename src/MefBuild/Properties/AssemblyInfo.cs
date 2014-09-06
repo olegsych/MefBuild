15bd4324ccd89df607e6025f2fe60cb2f7c82276
Oleg Sych <oleg_sych@hotmail.com>
2014-09-06 11:21:11 -0700

    - Enabled assembly signing in public releases. - Fixed FxCop errors.

--- src/MefBuild/Properties/AssemblyInfo.cs
+++ src/MefBuild/Properties/AssemblyInfo.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Composable Build Commands")]
-[assembly: AssemblyCompany("Oleg Sych")]
-[assembly: AssemblyDescription("Composable Build Commands")]
-[assembly: AssemblyVersion("0.1.*")]
+// Major version of assembly should match the major version of the NuGet package.
+// Leave minor, build and revision numbers of the assembliy as 0 for compatibility 
+// with third-party assemblies with stron-name references to this assembly.
+[assembly: AssemblyVersion("1.0.0.0")]
 
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
-
-[assembly: InternalsVisibleTo("MefBuild.Tests")]
