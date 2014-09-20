feca422485c547a2cbc32a24f7aaf57ef7faa73e
Oleg Sych <oleg_sych@hotmail.com>
2014-09-19 21:20:33 -0700

    - Renamed MefBuild assembly to MefBuild.Framework to enable creating MefBuild.exe.

--- src/Framework/Properties/AssemblyInfo.cs
+++ src/Framework/Properties/AssemblyInfo.cs
@@ -3,8 +3,8 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 // Major version of assembly should match the major version of the NuGet package.
-// Leave minor, build and revision numbers of the assembliy as 0 for compatibility 
-// with third-party assemblies with stron-name references to this assembly.
+// Leave minor, build and revision numbers of the assembly as 0 for compatibility 
+// with third-party assemblies with strong-name references to this assembly.
 [assembly: AssemblyVersion("1.0.0.0")]
 
 [assembly: ComVisible(false)]
