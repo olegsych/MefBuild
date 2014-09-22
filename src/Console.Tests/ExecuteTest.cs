f4cff01a6e72d5795319adf6e170714435f428bb
Oleg Sych <oleg_sych@hotmail.com>
2014-09-22 06:47:00 -0700

    - Defined composite Execute command to encapsulate the default "execute" verb of MefBuild.exe.

---
+++ src/Console.Tests/ExecuteTest.cs
@@ -0,0 +1,38 @@
+﻿using System.Composition;
+using System.Composition.Hosting;
+using System.Linq;
+using Xunit;
+
+namespace MefBuild
+{
+    public static class ExecuteTest
+    {
+        [Fact]
+        public static void ClassIsInternalAndNotMeantToBeUsedDirectly()
+        {
+            Assert.False(typeof(Execute).IsPublic);
+        }
+
+        [Fact]
+        public static void ClassInheritsFromCommandToParticipateInBuildProcess()
+        {
+            Assert.True(typeof(Command).IsAssignableFrom(typeof(Execute)));
+        }
+
+        [Fact]
+        public static void ClassIsExportedSharedToExecuteOnlyOnceDuringBuild()
+        {
+            CompositionContext context = new ContainerConfiguration().WithPart<Execute>().CreateContainer();
+            var instance1 = context.GetExport<Execute>();
+            var instance2 = context.GetExport<Execute>();
+            Assert.Same(instance1, instance2);
+        }
+
+        [Fact]
+        public static void ClassDefinesExecutionOrderOfCommandsItDependsOn()
+        {
+            DependsOnAttribute attribute = typeof(Execute).GetCustomAttributes(false).OfType<DependsOnAttribute>().Single();
+            Assert.Equal(new[] { typeof(LoadAssemblies), typeof(ResolveCommandTypes), typeof(ExecuteCommands) }, attribute.DependencyCommandTypes);
+        }
+    }
+}
---
+++ src/Console/Execute.cs
@@ -0,0 +1,9 @@
+﻿using System.Composition;
+
+namespace MefBuild
+{
+    [Export, Shared, DependsOn(typeof(LoadAssemblies), typeof(ResolveCommandTypes), typeof(ExecuteCommands))]
+    internal class Execute : Command
+    {
+    }
+}
