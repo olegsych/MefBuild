0a82d2ae010e63a384d0b3e129692d440fb877e1
Oleg Sych <oleg_sych@hotmail.com>
2014-09-27 10:19:52 -0700

    - Defined Record class to encapsulate a log record.

---
+++ src/Framework.Tests/Hosting/RecordTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Xunit;
+
+namespace MefBuild.Hosting
+{
+    public class RecordTest
+    {
+        [Fact]
+        public void ClassIsPublicForOutputImplementations()
+        {
+            Assert.True(typeof(Record).IsPublic);
+        }
+
+        [Fact]
+        public void ClassIsSealedBecauseItEncapsulatesAllVariationsOfLogRecords()
+        {
+            Assert.True(typeof(Record).IsSealed);
+        }
+
+        [Fact]
+        public void ConstructorInitializesAllProperties()
+        {
+            var record = new Record("Test Text", EventType.Error, EventImportance.High);
+            Assert.Equal("Test Text", record.Text);
+            Assert.Equal(EventType.Error, record.RecordType);
+            Assert.Equal(EventImportance.High, record.Importance);
+        }
+
+        [Fact]
+        public void ConstructorThrowsArgumentNullExceptionWhenTextIsNullToPreventUsageErrors()
+        {
+            var e = Assert.Throws<ArgumentNullException>(() => new Record(null, default(EventType), default(EventImportance)));
+            Assert.Equal("text", e.ParamName);
+        }
+
+        [Fact]
+        public void ConstructorAllowsEmptyTextSoThatBlankRecordsCanBeUsedToMakeOutputMoreReadable()
+        {
+            var record = new Record(string.Empty, default(EventType), default(EventImportance));
+            Assert.Equal(string.Empty, record.Text);
+        }
+
+        [Fact]
+        public void PropertiesAreReadOnlyBecauseRecordInstanceIsImmutable()
+        {
+            Assert.Null(typeof(Record).GetProperty("Text").SetMethod);
+            Assert.Null(typeof(Record).GetProperty("Type").SetMethod);
+            Assert.Null(typeof(Record).GetProperty("Importance").SetMethod);
+        }
+    }
+}
---
+++ src/Framework/Hosting/Record.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace MefBuild.Hosting
+{
+    /// <summary>
+    /// Encapsulates a <see cref="Log"/> record.
+    /// </summary>
+    public sealed class Record
+    {
+        private readonly string text;
+        private readonly EventType recordType;
+        private readonly EventImportance importance;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Record"/> class with the specified <paramref name="text"/>, 
+        /// <paramref name="recordType"/> and <paramref name="importance"/>.
+        /// </summary>
+        public Record(string text, EventType recordType, EventImportance importance)
+        {
+            if (text == null)
+            {
+                throw new ArgumentNullException("text");
+            }
+
+            this.text = text;
+            this.recordType = recordType;
+            this.importance = importance;
+        }
+
+        /// <summary>
+        /// Gets the record text.
+        /// </summary>
+        public string Text 
+        {
+            get { return this.text; }
+        }
+
+        /// <summary>
+        /// Gets the record type.
+        /// </summary>
+        public EventType RecordType 
+        {
+            get { return this.recordType; }
+        }
+
+        /// <summary>
+        /// Gets the record importance.
+        /// </summary>
+        public EventImportance Importance 
+        {
+            get { return this.importance; }
+        }
+    }
+}
