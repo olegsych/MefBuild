337f630cef70873590299ce53259c8e86554da25
Oleg Sych <oleg_sych@hotmail.com>
2014-10-10 10:51:29 +1300

    - Created ParameterAttribute to supply metadata describing command parameters.

---
+++ src/Framework.Tests/ParameterAttributeTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Composition.Hosting;
+using System.Composition.Hosting.Core;
+using System.Linq;
+using System.Reflection;
+using Xunit;
+
+namespace MefBuild
+{
+    public class ParameterAttributeTest
+    {
+        [Fact]
+        public void ClassIsMetadataAttributeThatCanBeAppliedToPropertiesAndParameters()
+        {
+            Assert.True(typeof(ParameterAttribute).IsPublic);
+            Assert.True(typeof(Attribute).IsAssignableFrom(typeof(ParameterAttribute)));
+            Assert.NotNull(typeof(ParameterAttribute).GetCustomAttribute<MetadataAttributeAttribute>());
+            Assert.Equal(
+                AttributeTargets.Parameter | AttributeTargets.Property,
+                typeof(ParameterAttribute).GetCustomAttribute<AttributeUsageAttribute>().ValidOn);
+        }
+
+        [Fact]
+        public void NameProvidesNameMetadata()
+        {
+            IEnumerable<KeyValuePair<string, object>> metadata = GetImportContractMetadata<TestCommand>();
+            Assert.Contains(new KeyValuePair<string, object>("Name", "Test Parameter"), metadata);
+        }
+
+        [Fact]
+        public void SummaryProvidesSummaryMetadata()
+        {
+            IEnumerable<KeyValuePair<string, object>> metadata = GetImportContractMetadata<TestCommand>();
+            Assert.Contains(new KeyValuePair<string, object>("Summary", "Test Summary"), metadata);            
+        }
+
+        private static IEnumerable<KeyValuePair<string, object>> GetImportContractMetadata<T>() where T : new()
+        {
+            CompositionContract importContract = null;
+            var provider = new StubExportDescriptorProvider();
+            provider.OnGetExportDescriptors = (contract, accessor) =>
+            {
+                importContract = contract;
+                return Enumerable.Empty<ExportDescriptorPromise>();
+            };
+
+            var container = new ContainerConfiguration()
+                .WithProvider(provider)
+                .CreateContainer();
+
+            container.SatisfyImports(new T());
+
+            return importContract.MetadataConstraints;
+        }
+
+        public class TestCommand : Command
+        {
+            [Import(AllowDefault = true), 
+            Parameter(Name = "Test Parameter", Summary = "Test Summary")]
+            public int Property { get; set; }
+        }
+
+        public class StubExportDescriptorProvider : ExportDescriptorProvider
+        {
+            public Func<CompositionContract, DependencyAccessor, IEnumerable<ExportDescriptorPromise>> OnGetExportDescriptors { get; set; }
+
+            public override IEnumerable<ExportDescriptorPromise> GetExportDescriptors(CompositionContract contract, DependencyAccessor descriptorAccessor)
+            {
+                return this.OnGetExportDescriptors(contract, descriptorAccessor);
+            }
+        }
+    }
+}
---
+++ src/Framework/ParameterAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Composition;
+
+namespace MefBuild
+{
+    /// <summary>
+    /// Provides metadata describing an input parameter of a command.
+    /// </summary>
+    [MetadataAttribute, AttributeUsage(AttributeTargets.Parameter | AttributeTargets.Property)]
+    public sealed class ParameterAttribute : Attribute
+    {
+        /// <summary>
+        /// Gets or sets the name of the parameter.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the summary description of the parameter.
+        /// </summary>
+        public string Summary { get; set; }
+    }
+}
