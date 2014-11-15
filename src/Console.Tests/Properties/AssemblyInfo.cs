47fe73cae343ec06f58cfafe0991f5e2c3c84fe8
Oleg Sych <oleg_sych@hotmail.com>
2014-11-15 13:41:57 -0800

    - Disabled parallel execution of Xunit tests to prevent test failures caused by static Console and Debug classes. - Added Visual Studio 2015 *.sln.ide folders to the .gitignore list.

--- src/Console.Tests/Properties/AssemblyInfo.cs
+++ src/Console.Tests/Properties/AssemblyInfo.cs
@@ -1,5 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using Xunit;
 
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
+[assembly: CollectionBehavior(MaxParallelThreads = 1)]
